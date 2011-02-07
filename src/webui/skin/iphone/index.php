b89af63d10836ce53bbba99a6bc9a7313f3901fb
SmashD <SmashD@gmail.com>
2011-02-07 14:19:10 +0000

    - changed all items positions and scalings to get a fixed viewport zoom of 1.0 (no zoom) - added Video/Music/Photo/All to the coloured buttons and changed their colour to a less eye-hurting one ;) - using jqtouch for an iPhone-like menu-overlay when pressing the power button, gives options "Standby Toggle", "Reboot" and "Power Off" (can't get power off working atm) - using jqtouch for an iPhone-like about overlay - added current tar of v2.1 to misc/c200remote_2.1.zip

--- src/webui/skin/iphone/index.php
+++ src/webui/skin/iphone/index.php
@@ -5,7 +5,7 @@ $html = <<<EOHTML
 
 <title>C200 iRemote</title>
 
-<meta name="viewport" content="width=device-width; user-scalable=0; initial-scale=1.17, maximum-scale=1.17">
+<meta name="viewport" content="width=device-width; user-scalable=0; initial-scale=1.0, maximum-scale=1.0">
 <meta name="apple-mobile-web-app-capable" content="yes">
 
 <meta name="author" content="dc11ab" />
@@ -18,14 +18,75 @@ $html = <<<EOHTML
 
 <script language="javascript" type="text/javascript" src="../../control.js"></script>
 
+
+<!-- jQTouch Start -->
+<script src="jqtouch/jquery.1.3.2.min.js" type="application/x-javascript" charset="utf-8"></script>
+<script src="jqtouch/jqtouch.min.js" type="application/x-javascript" charset="utf-8"></script>
+<script src="jqtouch/jqtouch.transitions.js" type="application/x-javascript" charset="utf-8"></script>
+<style type="text/css" media="screen">@import "jqtouch/jqtouch.css";</style>
+<style type="text/css" media="screen">@import "themes/apple/theme.css";</style>
+
+<script type="text/javascript" charset="utf-8">
+    var jQT = new $.jQTouch({
+        icon: 'images/apple-touch-icon-precomposed.png',
+        addGlossToIcon: false,
+        startupScreen: 'images/apple-touch-startup-image.png',
+        statusBar: 'black',
+        fixedViewport: 'true',
+        fullScreen: 'true',
+        useAnimations: 'true',
+        preloadImages: [
+            'themes/apple/img/back_button.png',
+            'themes/apple/img/back_button_clicked.png',
+            'themes/apple/img/button_clicked.png',
+            'themes/apple/img/grayButton.png',
+            'themes/apple/img/whiteButton.png',
+            'themes/apple/img/loading.gif',
+            'themes/apple/img/toggle.png',
+            'themes/apple/img/toggleOn.png',
+            'themes/apple/img/toolbar.png',
+            'themes/apple/img/thumb.png',
+            'images/apple-touch-icon.png',
+            'images/apple-touch-icon-precomposed.png',
+            'images/apple-touch-startup-image.png',
+            'images/saf_41.gif',
+            'images/saf_42.gif'
+            ]
+    });
+</script>
+<!--jQTouch End -->
+
+<!-- special reboot/shutdown script -->
+<script type="text/javascript" charset="utf-8">
+function start_reboot() {
+	passfCommand('power');
+	setTimeout( "end_reboot();", 500); 
+}
+function end_reboot() {
+	passfCommand('eject');
+} 
+
+function start_poweroff() {
+//	return passfCommand('power');
+		alert("not working atm, sorry...");
+
+//	setTimeout( "end_poweroff();", 500); 
+}
+function end_poweroff() {
+	passfCommand('delete');
+} 
+</script>
+<!-- special reboot/shutdown script -->
+
 <style>
 div.section {
 background:#eef;
+/*background:#00f;*/ /*debug color*/
 padding: 0.1em;
-border-width:0px;
+border-width:0px; /*later adjustment easier*/
 border-style:solid;
 border-color:grey;
-width:265px;
+width:310px;
 margin-left:auto;
 margin-top:2px;
 margin-right:auto;
@@ -33,67 +94,67 @@ float:left;
 }
 div.section_a_left {
 background:#eef;
+/*background:#0f0;*/ /*debug color*/
 padding: 0.1em;
 border-width:0px;
 border-style:solid;
 border-color:grey;
-width:200px;
-margin-left:auto;
+width:220px;
+margin-left:0px;
 margin-top:2px;
-margin-right:auto;
 float:left;
 }
 div.section_a_right {
 background:#eef;
+/*background:#f00;*/ /*debug color*/
 padding: 0.1em;
 border-width:0px;
 border-style:solid;
 border-color:grey;
-width:50px;
-margin-left:auto;
+width:84px;
 margin-top:2px;
-margin-right:4px;
-float:right;
+margin-right:0px;
+float:left;
+text-align: right;
 }
 button.vtext {
-margin-left: 5%;
-width: 55px;
-height: 26px;
+width: 80px;
+height: 31px;
 }
 button.standard {
 width:64px;
 height:20px;
-padding: 0.1em;
+padding: 0.15em;
 margin-left:auto;
 margin-right:auto;
 }
 button.small {
-width:64px;
-height:20px;
-padding: 0.1em;
+width:70px;
+height:25px;
+padding: 0.25em;
 margin-left:auto;
 margin-right:auto;
 text-align:left;
-text-indent:12px;
+text-indent:6px;
 }
 button.three_row {
-width:84px;
-height:22px;
-padding: 0.1em;
+width:99px;
+height:28px;
+padding: 0.25em;
 margin-left:auto;
 margin-right:auto;
 }
 button.four_row {
-width:62px;
-height:20px;
-padding: 0.1em;
+width:73px;
+height:23px;
+padding: 0.2em;
 margin-left:auto;
 margin-right:auto;
 }
 button.five_row {
-width:49px;
-height:20px;
-padding: 0.1em;
+width:58px;
+height:25px;
+padding: 0.25em;
 }
 div.c200_remote_logo {
 float: left;
@@ -102,11 +163,45 @@ margin-right: auto;
 width:57px;
 height:57px;
 background-image: url(images/apple-touch-icon-precomposed.png); 
+text-align:center;
 }
 
 #buttons {
 background-color:silver;
+color: black;
+font-family: arial, verdana, ms sans serif;
+font-weight: bold;
+font-size: 11pt; 
 }
+#buttons_small {
+background-color:silver;
+color: black;
+font-family: arial, verdana, ms sans serif;
+font-weight: bold;
+font-size: 9pt; 
+}
+
+a.five_row:link { /*Power "button" styling*/
+	text-decoration:none;
+	color:#000000;
+	border-color:#000000;
+	border-style:solid;
+	border-width:1px;
+	padding-top:4px;
+	padding-left:4px;
+	padding-right:4px;
+	padding-bottom:5px;
+}
+a.about_button:link { /*about "button" styling*/
+	text-decoration:none;
+	color:#333399;
+	font-family: arial, verdana, ms sans serif;
+	font-weight: bold;
+	font-size: 12pt;
+	margin-left:-10px; 
+}
+
+
 <!-- /* :hover is a pseudo selector to use to set the mouseover attributes */ -->
 #buttons:hover {
 background-color: #F9F9F9; }
@@ -117,63 +212,70 @@ background-color: #F9F9F9; }
 
 <div id="RemoteArea" style="padding: 0.1em; height: 540px; width: 260px; margin-left: 0px; margin-top: 0px;">
 <div class="section" name="SectionA"> 
-	<button class="five_row" id="buttons" style="background-color: #CC0000;" onclick="return passCommand('x')">POWER</button>
-	<button class="five_row" id="buttons" onclick="return passfCommand('tvmode')">TV MOD</button>
-	<button class="five_row" id="buttons" onclick="return passfCommand('setup')">SETUP</button>
-	<button class="five_row" id="buttons" onclick="return passfCommand('btsuspend')">BT SUS</button>
-	<button class="five_row" id="buttons" onclick="return passCommand('j')">EJECT</button> 
+	<!-- have to use a styled href here, does not work with buttons :( -->
+	<a class="five_row slideup" id="buttons_small" style="background-color:#cc2222;" href="#PowerPanel">POWER</a>
+
+	<button class="five_row" id="buttons_small" onclick="return passfCommand('tvmode')">TV MOD</button>
+	<button class="five_row" id="buttons_small" onclick="return passfCommand('setup')">SETUP</button>
+	<button class="five_row" id="buttons_small" onclick="return passfCommand('btsuspend')">BT SUS</button>
+	<button class="five_row" id="buttons_small" onclick="return passCommand('j')">EJECT</button> 
 </div>
-<div class="section_a_left" name="SectionB"> 
-	<button class="small" id="buttons" onclick="return passCommand('1')">1&emsp;.:/@</button>
-	<button class="small" id="buttons" onclick="return passCommand('2')">2&emsp;abc</button>
-	<button class="small" id="buttons" onclick="return passCommand('3')">3&emsp;def</button>
-	<br>
-	<button class="small" id="buttons" onclick="return passCommand('4')">4&emsp;ghi</button>
-	<button class="small" id="buttons" onclick="return passCommand('5')">5&emsp;jkl</button>
-	<button class="small" id="buttons" onclick="return passCommand('6')">6&emsp;mno</button>
-	<br>
-	<button class="small" id="buttons" onclick="return passCommand('7')">7&emsp;pqrs</button>
-	<button class="small" id="buttons" onclick="return passCommand('8')">8&emsp;tuv</button>
-	<button class="small" id="buttons" onclick="return passCommand('9')">9&emsp;def</button>
-	<br>
-	<button class="small" id="buttons" onclick="return passCommand('c')">DEL</button>
-	<button class="small" id="buttons" onclick="return passCommand('0')">0&nbsp;[spc]</button>
-	<button class="small" id="buttons" onclick="return passCommand('l')">C/NUM</button>
-	<br>
-	<button class="small" id="buttons" onclick="return passCommand('O')">HOME</button>
-	<button class="small" id="buttons" onclick="return passCommand('g')">FILE&nbsp;M</button>
-	<button class="small" id="buttons" onclick="return passCommand('H')">T SEEK</button>
+<div style="float:left; width:310px;">
+	<div class="section_a_left" name="SectionB"> 
+		<button class="small" id="buttons" onclick="return passCommand('1')">1&nbsp;&nbsp;.:/@</button>
+		<button class="small" id="buttons" onclick="return passCommand('2')">2&nbsp;&nbsp;abc</button>
+		<button class="small" id="buttons" onclick="return passCommand('3')">3&nbsp;&nbsp;def</button>
+		<br>
+		<button class="small" id="buttons" onclick="return passCommand('4')">4&nbsp;&nbsp;ghi</button>
+		<button class="small" id="buttons" onclick="return passCommand('5')">5&nbsp;&nbsp;jkl</button>
+		<button class="small" id="buttons" onclick="return passCommand('6')">6&nbsp;&nbsp;mno</button>
+		<br>
+		<button class="small" id="buttons" onclick="return passCommand('7')">7&nbsp;&nbsp;pqrs</button>
+		<button class="small" id="buttons" onclick="return passCommand('8')">8&nbsp;&nbsp;tuv</button>
+		<button class="small" id="buttons" onclick="return passCommand('9')">9&nbsp;&nbsp;def</button>
+		<br>
+		<button class="small" id="buttons" onclick="return passCommand('c')">DEL</button>
+		<button class="small" id="buttons" onclick="return passCommand('0')">0&nbsp;[spc]</button>
+		<button class="small" id="buttons" onclick="return passCommand('l')">C/NUM</button>
+		<br>
+		<button class="small" id="buttons" onclick="return passCommand('O')">HOME</button>
+		<button class="small" id="buttons" onclick="return passCommand('g')">FILE&nbsp;M</button>
+		<button class="small" id="buttons" onclick="return passCommand('H')">T SEEK</button>
+	</div>
+	<div class="section_a_right" name="SectionC">
+		<button class="vtext" id="buttons" style="background-color: #c22;" onclick="return passfCommand('red')">VIDEO</button>
+		<button class="vtext" id="buttons" style="background-color: #2c2;" onclick="return passfCommand('green')">MUSIC</button>
+		<button class="vtext" id="buttons" style="background-color: #cc2;" onclick="return passfCommand('yellow')">PHOTO</button>
+		<button class="vtext" id="buttons" style="background-color: #22c;" onclick="return passfCommand('blue')">ALL</button>
+	</div>
 </div>
-<div class="section_a_right" name="SectionC">
-	<button class="vtext" id="buttons" style="background-color: red;" onclick="return passfCommand('red')">&nbsp;</button>
-	<button class="vtext" id="buttons" style="background-color: green;" onclick="return passfCommand('green')">&nbsp;</button>
-	<button class="vtext" id="buttons" style="background-color: yellow;" onclick="return passfCommand('yellow')">&nbsp;</button>
-	<button class="vtext" id="buttons" style="background-color: blue;" onclick="return passfCommand('blue')">&nbsp;</button>
-</div>
+
 <div class="section" name="SectionE">
 	<button class="three_row" id="buttons" onclick="return passCommand('v')">RETURN</button>
-	<button class="three_row" id="buttons" style="background-color: #999;" onclick="return passCommand('U')">&uArr;</button>
+	<button class="three_row" id="buttons" style="background-color: #777;" onclick="return passCommand('U')">UP</button>
 	<button class="three_row" id="buttons" onclick="return passCommand('B')">SOURCE</button>
 	<br>
-	<button class="three_row" id="buttons" style="background-color: #999;" onclick="return passCommand('L')">&lArr;</button>
+	<button class="three_row" id="buttons" style="background-color: #777;" onclick="return passCommand('L')">LEFT</button>
 	<button class="three_row" id="buttons" style="background-color: #777;" onclick="return passfCommand('enter')">OK/Enter</button>
-	<button class="three_row" id="buttons" style="background-color: #999;" onclick="return passCommand('R')">&rArr;</button>
+	<button class="three_row" id="buttons" style="background-color: #777;" onclick="return passCommand('R')">RIGHT</button>
 	<br>
 	<button class="three_row" id="buttons" onclick="return passCommand('i')">INFO</button>
-	<button class="three_row" id="buttons" style="background-color: #999;" onclick="return passCommand('D')">&dArr;</button>
+	<button class="three_row" id="buttons" style="background-color: #777;" onclick="return passCommand('D')">DOWN</button>
 	<button class="three_row" id="buttons" onclick="return passCommand('s')">STOP</button>
 </div>
 
-<div style="float:left; width:265px;">
+<div style="float:left; width:310px;">
 	
-	<div class="section" name="SectionF" style="width:99px;">
+	<div class="section" name="SectionF" style="width:123px;">
 		<button class="three_row" style="height:27px;" id="buttons" onclick="return passfCommand('volup')">VOL&nbsp;+</button><br>
 		<button class="three_row" style="height:27px;" id="buttons" onclick="return passfCommand('voldown')">VOL&nbsp;&ndash;</button>
 	</div>	
 
-	<div class="c200_remote_logo" name="logo"></div>
+	<div class="c200_remote_logo" name="logo">
+		<br><br><a class="about_button slideup" href="#AboutPanel">about</a>
+	</div>
 
-	<div class="section" name="SectionG" style="width:99px; float:right; margin-right:-12px;">
+	<div class="section" name="SectionG" style="width:117px; float:right; margin-right:-15px;">
 		<button class="three_row" style="height:27px;" id="buttons" onclick="return passCommand('y')">PLAY</button><br>
 		<button class="three_row" style="height:27px;" id="buttons" onclick="return passCommand('p')">PAUSE</button>
 	</div>
@@ -202,27 +304,64 @@ background-color: #F9F9F9; }
 <!-- The end of original rc) -->
 <div class="section" id="macro" style="font-family: sans-serif; font-size:0.7em;">
 	<form action="return passCommand('$macroinput')" id="macroForm" name="macroinput" method="post" enctype="multipart/form-data">
-		<input size="27" value="Insert macro..." name="macroinput" onblur="if (this.value == '') {this.value = 'Insert macro...';}" onfocus="if (this.value == 'Insert macro...') {this.value = '';}" type="text" style="width:192px;">
-		<button type="four_row" class="standard" id="buttons"><b>SUBMIT</b></button>
-
-		<b>Info:</b><br>
-		This website is being viewed in fullscreen when you launch it as an icon from your springboard!<br>
-		Simply save it to an icon from your Safari menu:<br>
-		<br>
-		4.1 and older:<img src="images/saf_41.gif" style="float:right; margin-right:100px;" /><br>
-		<br>
-		<br>
-		4.2.1+:<img src="images/saf_42.gif" style="float:right; margin-right:100px;" /><br>
-		<br>
-		<br>
-		As an alternative you may use any 3rd party webbrowser that supports fullscreen view.<br>
-		<br>
-		<br>
-		iPhone web remote for Syabas Popcorn Hour C-200<br>
-		<i>edited by <a href="http://smashd.de" target="_blank">smashd.de</a>, ver 1.1, Feb 07 2011</i>
+		<input size="30" value="Insert macro..." name="macroinput" onblur="if (this.value == '') {this.value = 'Insert macro...';}" onfocus="if (this.value == 'Insert macro...') {this.value = '';}" type="text" style="width:225px;">&nbsp;&nbsp;&nbsp;
+		<button type="four_row" style="width:67px;height:17px;padding-top:-0.3em;font-size:7pt;margin-top:-4px;" class="standard" id="buttons"><b>RUN</b></button>
+    <div class="info">
+				<p style="margin-top:-7px;">Info:</p>
+        <p>Add this page to your home screen / springboard to view the custom icon, 
+        	<br>startup screen, and full screen mode.</p>
+				<br>
+				<p>Simply save it to an icon from your Safari menu:<br>
+				<br>
+				4.1 and older:<img src="images/saf_41.gif" style="float:right; margin-right:100px;" /><br>
+				<br>
+				<br>
+			4.2.1+:<img src="images/saf_42.gif" style="float:right; margin-right:100px;" /></p>
+		    </div>
+		    <ul>
+		        <li><a class="slideup" href="#AboutPanel">about</a></li>
+		    </ul>
 	</form>
 </div>
 </div>
+
+<!-- special iPhone-style layers -->
+
+<div id="PowerPanel" style="margin-left:-5px;width:320px;background: rgb(197,204,211) url(themes/apple/img/pinstripes.png);">
+    <div class="toolbar">
+        <h1>Power Options</h1>
+        <a class="button back" href="#">Back</a>
+    </div>
+        <ul>
+            <li><a onclick="return passfCommand('power')" href="#">Standby Toggle</a></li>
+            <li><a onclick="start_reboot();" href="#">Reboot</a></li>
+            <li><a onclick="start_poweroff();" href="#">Power Off</a></li>
+        </ul>
+</div>
+
+<div id="AboutPanel" style="margin-left:-5px;width:320px;background: rgb(197,204,211) url(themes/apple/img/pinstripes.png);">
+    <div class="toolbar">
+        <h1>C200 iRemote</h1>
+        <a class="button back" href="#">Back</a>
+    </div> 
+    <div class="info">
+    	<p style="text-align:center;"><img src="images/apple-touch-icon-precomposed.png" /></p>
+			<br>
+			<p style="margin-left:15px;"><strong>c200remote</strong> skin "C200 iRemote" by <a href="http://smashd.de" target="_blank">smashd.de</a>
+    </div>
+	        <ul>
+	            <li><a href="http://code.google.com/p/c200remote/" target="_blank">google code project page</a></li>
+	            <li>(c200remote svn and more)</li>
+	        </ul>
+	        <ul>
+	            <li><a href="http://www.networkedmediatank.com/showthread.php?tid=30168" target="_blank">forum thread</a></li>
+	            <li>(at networkedmediatank.com)</li>
+	        </ul>
+			</p>
+		</p>
+</div>
+
+
 </body>
 </html>
 
